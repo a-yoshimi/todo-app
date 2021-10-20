e421941e06f2d42a73851aee4c0451d0dc517b6b
Yoshimi Amano <yoshimi.amano@nextbeat.net>
2021-10-20 16:38:48 +0900

    wip: Acrtion asyncの活用

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -5,11 +5,11 @@
  */
 package controllers
 import java.sql.Timestamp
-
-import scala.concurrent.duration._
-import scala.concurrent._
-
 import javax.inject._
+import scala.concurrent.duration._
+import scala.concurrent.{Future, _}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import play.api.mvc._
 import model._
 import lib.persistence.default.{TodoRepository,TodoCategoryRepository}
@@ -18,55 +18,37 @@ import lib.model.Todo._
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def index() = Action { implicit req =>
-
-    // カテゴリ一覧の取得
-    val categoryList = Await.result(TodoCategoryRepository.fecheAll(), Duration.Inf).map(category =>
-      TodoCategory(
-        category.id,
-        category.v.name,
-        category.v.color
-      )
-    )
-    println("a")
-    // todo一覧の取得
-      val todoList  = Await.result(TodoRepository.fecheAll(), Duration.Inf).map(todo =>
-      Todo(
-        todo.id,
-        categoryList.find(category =>
-          category.id == todo.v.categoryId).getOrElse(
-          Some(
+  def index() = Action.async {
+    for (
+      category <- TodoCategoryRepository.fecheAll();
+      todo     <- TodoRepository.fecheAll()
+    ) yield {
+      val categoryList = category.map(category =>
             TodoCategory(
-              todo.v.categoryId,
-              "カテゴリなし",
-              0
+              category.id,
+              category.v.name,
+              category.v.color
             )
-          ).get
-        ).name,
-        todo.v.title,
-        todo.v.body,
-        todo.v.state,
+          )
+      val todoList = todo.map(todo =>
+        Todo(
+          todo.id,
+          categoryList.find(category =>
+            category.id == todo.v.categoryId).getOrElse(TodoCategory(todo.v.categoryId, "カテゴリなし", 0)).name,
+          todo.v.title,
+          todo.v.body,
+          todo.v.state
+        )
       )
-    )
 
-
-    val vv = ViewValueHome(
-      title        = "Todo管理",
-      cssSrc       = Seq("main","category","list","todo").map(s=> s  + ".css"),
-      jsSrc        = Seq("main.js"),
-      categoryList = categoryList,
-      todoList     = todoList
-    )
-    Ok(views.html.Home(vv))
+      val vv = ViewValueHome(
+        title = "Todo管理",
+        cssSrc = Seq("main", "category", "list", "todo").map(s => s + ".css"),
+        jsSrc = Seq("main.js"),
+        categoryList = categoryList,
+        todoList = todoList
+      )
+      Ok(views.html.Home(vv))
+    }
   }
-
-//      Await.result(TodoRepository.fecheAll(), Duration.Inf).map{ todo =>
-//      println(todo.id)
-//      Todo(
-//        todo.id.toInt,
-//        2,
-//        "A",
-//        "内容",
-//        lib.model.Todo.TodoStatus.IS_TODO
-//      )
 }
